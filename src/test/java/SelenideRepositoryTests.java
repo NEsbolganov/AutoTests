ab14a25cc9e869b419e15d567ef6ce572778e3f9
Esolganov Nartay <esbolganovnartai@gmail.com>
2024-01-23 13:11:54 +0600

    7 видео и ДЗ по нему

--- src/test/java/SelenideRepositoryTests.java
+++ src/test/java/SelenideRepositoryTests.java
@@ -1,4 +1,6 @@
+import com.codeborne.selenide.Browser;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -51,5 +53,30 @@ public class SelenideRepositoryTests {
         $("[id=wiki-content]").$(byText("Soft assertions")).click();
 
         $(".markdown-body").shouldHave(text("JUnit5"));
+    }
+
+    @Test
+    void findElementByHoverMethod(){
+        open("https://github.com/");
+
+//        $(".list-style-none").$(byText("Solutions")).click();
+        $(".list-style-none").$(byText("Solutions")).hover();
+
+        $("[aria-labelledby=solutions-for-heading]").$(byText("Enterprise")).click();
+
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered"));
+    }
+
+    @Test
+    void testDragAndDrop(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        sleep(5000);
+
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+
+        $("#column-a").shouldHave(text("B"));
+
+        $("#column-b").shouldHave(text("A"));
     }
 }
